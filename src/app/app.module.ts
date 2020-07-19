700c1b36aeae284392660fc62503473c9b144cb9
Stephen Klop <stefklop18@gmail.com>
2020-07-19 20:57:10 +0200

    Angular init & Started menubar / jumotron component

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenubarComponent } from './components/menubar/menubar.component';
+import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenubarComponent,
+    JumbotronComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
